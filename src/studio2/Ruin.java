b5c7d602d2423010689f34cf1e23c78150ce2cd7
viri <viri@DESKTOP-HQGVONP.dhcp.wustl.edu>
2025-01-29 14:17:38 -0600

    studio 2 complete

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,57 @@
+package studio2;
+
+import java.util.Scanner; 
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("How many days do you want to play? ");
+		int totalSimulations = in.nextInt();
+
+		System.out.print("Enter Start Amount: ");
+		double startAmount = in.nextDouble();
+
+		System.out.print("Enter Win Chance: ");
+		double winChance = in.nextDouble();
+
+		System.out.print("Enter Win Limit: ");
+		double winLimit = in.nextDouble();
+
+		double i = startAmount;
+
+		for (int j = 0; j < totalSimulations; j++) {
+			
+
+			while (i < winLimit && i > 0) {
+				double rand = Math.random();
+				int integernum = (int) Math.round(rand * 100);
+				int counter = 0;
+
+				if(integernum <= winChance) {
+					i++;
+					counter++;
+				}
+				else {
+					i--;
+					counter++;
+				}
+				
+				if (i == 1) {
+					int done = j + 1;
+					System.out.println("Simulation " + done + ": " + counter + " LOSE");
+				}
+				
+				else if (i == winLimit - 1) {
+					int done = j + 1;
+					System.out.println("Simulation " + done + ": " + counter + " WIN");
+				}
+			}
+			
+			i = startAmount;
+		}
+
+	}
+
+}
